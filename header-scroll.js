9d77f50590a431298f8529ab8e5cbdac80438bdc
aibekbbic <129316740+aibekbbic@users.noreply.github.com>
2023-04-07 21:42:34 +0200

    Add files via upload

---
+++ header-scroll.js
@@ -0,0 +1,6 @@
+// window.addEventListener("scroll", function () {
+//   document.querySelector("header").style.backgroundSize =
+//     120 - +window.pageYOffset / 12 + "%";
+//   document.querySelector("header").style.opacity =
+//     1 - +window.pageYOffset / 700 + "";
+// });
